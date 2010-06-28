d9ecdfab083fbb633e9ef55e33ec66cd59a228f7
snowcloud <derek@snowcloud.co.uk>
2010-06-28 21:05:22 +0100

    added filter to use settings.SYNONYMS to allow site-specific resource names

--- templatetags/ct_tags.py
+++ templatetags/ct_tags.py
@@ -1,9 +1,18 @@
 from django import template
+from django.utils.translation import ugettext as _
 
 
 register = template.Library()
 
+# try:
+from django.conf import settings
+
 
 @register.filter
 def rating_display(value):
-	return 'rating- %d' % value
+    return 'rating- %d' % value
+
+@register.filter
+def site_resource_name(value):
+    v = getattr(settings, 'SYNONYMS', {})
+    return _(v.get(value, value))
