a7041eccccafe1bf68bfbbfe03b3e539a71e5b69
snowcloud <derek@snowcloud.co.uk>
2010-10-25 22:01:00 +0100

    added setup info

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='ct-templates',
+    name='ct-template',
     version='0.1dev',
-    packages=['ct_templates'],
+    packages=['ct_template'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.rst').read(),
 )
